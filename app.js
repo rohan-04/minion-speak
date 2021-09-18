44ddfd2fded703ebe32ed3844ad6174b47692af1
rohan-04 <mathurrohan04@gmail.com>
2021-09-19 02:32:37 +0530

    feat: add minion api for translation

--- app.js
+++ app.js
@@ -1,3 +1,23 @@
 var btnTranslate = document.querySelector('#btn-translate');
+var txtInput = document.querySelector('#txt-input');
+var outputDiv = document.querySelector('#output');
 
-console.log(btnTranslate);
+var serverURL = 'https://api.funtranslations.com/translate/minion.json';
+
+function getTranslationURL(text) {
+	return serverURL + '?' + 'text=' + text;
+}
+
+function handleClick() {
+	var inputText = txtInput.value;
+	fetch(getTranslationURL(inputText))
+		.then((response) => {
+			response.json();
+		})
+		.then((response) => {
+			outputDiv.innerText = response.contents.translated;
+		})
+		.catch((error) => console.log(error));
+}
+
+btnTranslate.addEventListener('click', handleClick);
